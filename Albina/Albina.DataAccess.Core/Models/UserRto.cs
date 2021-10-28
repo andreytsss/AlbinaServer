53ab966cb121393e0dd45d440fd05fa76a2294f5
andreytsss <themarcocraft@mail.ru>
2021-10-28 19:23:49 +0300

    1

---
+++ Albina/Albina.DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Albina.DataAccess.Core.Models
+{
+    [Table("User")]
+    class UserRto
+    {
+      [Key]  public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumberPrefix { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
