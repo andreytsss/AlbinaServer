60a97db319f80a3d5c2f7ba50d2f6c4d1ad23b18
andreytsss <themarcocraft@mail.ru>
2021-11-09 19:25:05 +0300

    что-то новое

---
+++ Albina/Albina.DataAccess.Core/Models/VirtualExpenseRto.cs
@@ -0,0 +1,18 @@
+﻿using Albina.DataAccess.Core.Enums;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Albina.DataAccess.Core.Models
+{
+    [Table("VirtualExpense")]
+    public class VirtualExpenseRto
+    {
+        [Key] public int Id { get; set; }
+        public float Cost { get; set; }
+        public CostCurrencyRto CostCurrency { get; set; }
+        public VirtualExpenseStatusRto Status { get; set; }
+        public DataTimeOffset CreateDateTime { get; set; }
+        public List<DataTimeOffset> ChangeDateTimes { get; set; }
+    }
+}
