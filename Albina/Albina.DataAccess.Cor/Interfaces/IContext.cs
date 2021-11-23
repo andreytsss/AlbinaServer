9d487eac6bfb63c56b3351b8156f372cef733285
andreytsss <themarcocraft@mail.ru>
2021-11-23 19:30:06 +0300

    дада

--- Albina/Albina.DataAccess.Cor/Interfaces/IContext.cs
+++ Albina/Albina.DataAccess.Cor/Interfaces/IContext.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using Albina.DataAccess.Cor.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Albina.DataAccess.Cor.Interfaces
 {
     public  interface IContext : IDisposable, IAsyncDisposable
     {
-
+        DbSet<UserRto> Users { get; set;}
+        DbSet<VirtualExpenseRto> VirtualExpenses { get; set; }
+        DbSet<RejectExpenseRto> RejectExpenses { get; set; }
+        DbSet<AcceptExpenseRtp> AcceptExpenses { get; set; }
+        Task <int> SaveChangesAsync(CancellationToken cancellationToken = default)
     }
 }
